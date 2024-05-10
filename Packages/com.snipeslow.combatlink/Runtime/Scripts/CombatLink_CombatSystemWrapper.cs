2ea3b7725f83e22a20cd6d08a2d6d4c30bacdc0f
snipeslow <38489015+snipeslow@users.noreply.github.com>
2024-05-09 19:17:46 -0500

    Initial Upload

---
+++ Packages/com.snipeslow.combatlink/Runtime/Scripts/CombatLink_CombatSystemWrapper.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+#if UDONSHARP
+using UdonSharp;
+using VRC.SDKBase;
+using VRC.Udon;
+public class CombatLink_CombatSystemWrapper : UdonSharpBehaviour
+#else
+public class CombatLink_CombatSystemWrapper : MonoBehaviour
+#endif
+{
+    public CombatLinkManager CombatLinkManager;
+    [Header("CombatLink variables below!")]
+    public bool Active;
+    float MaxHealth = 0;
+    private void LateUpdate()
+    {
+        if (CombatLinkManager)
+        {
+            CombatLinkManager.Active = Active;
+            CombatLinkManager.Health = Networking.LocalPlayer.CombatGetCurrentHitpoints();
+            if(MaxHealth > CombatLinkManager.Health)
+            {
+                MaxHealth = CombatLinkManager.Health;
+            }
+            CombatLinkManager.MaxHealth = MaxHealth;
+        }
+    }
+    private void OnDisable()
+    {
+        MaxHealth = 0;
+    }
+}
